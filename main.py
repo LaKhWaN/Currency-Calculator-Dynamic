9a31fb887223b12237a7442f0ee920f45ad6a233
PROTHEMESH <78162039+PROTHEMESH@users.noreply.github.com>
2021-09-18 23:31:05 +0530

    project completed

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ htmlContent = r.content
 
 soup = BeautifulSoup(htmlContent,'html.parser')
 data = soup.find_all("div")
-# print(data)
-# exit()
+
 f = open("content.txt","w")
 f.write(str(soup.encode("utf-8")))
 f.close()
@@ -25,5 +24,18 @@ f.close()
 for line in lines:
     if "Indian Rupee" in line:
         data = line.split()
-        print(f"{to_curr} in Indian Rupee = "+data[data.index("Indian")-1].split(">")[1])
+        valcurr=data[data.index("Indian")-1].split(">")[1]
+        print(f"{to_curr} in Indian Rupee = "+valcurr)
         break
+
+ast=int(input(f"what do you want to do \n1 indian to {to_curr} \n2 {to_curr} to indian\n enter here:-> "))
+
+if ast==1:
+    x=int(input("enter total indian rupee domination:-> "))
+    concurr=x/float(valcurr)
+    print(str(round(concurr,2))+" "+to_curr)
+
+elif ast==2:
+    x=int(input(f"enter total {to_curr} domination:-> "))
+    concurr=x*float(valcurr)
+    print(str(round(concurr,2))+" "+"Indian Rupee")
