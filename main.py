24211ee6aa0724258598028879ec0965828bf4e4
LaKhWaN <lakhwanus009@gmail.com>
2021-09-18 23:09:45 +0530

    done all the basic
    Only some functions left

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from requests.models import ContentDecodingError
 
-to_curr = input("To which currency you want to convert:-> ")
+to_curr = input("Of Which country you want to know the currency:-> ")
 
 url = (f"https://www.google.com/search?q={to_curr}+to+inr")
 
@@ -11,21 +11,19 @@ r = requests.get(url)
 htmlContent = r.content
 
 soup = BeautifulSoup(htmlContent,'html.parser')
+data = soup.find_all("div")
+# print(data)
+# exit()
 f = open("content.txt","w")
-f.write(str(soup))
+f.write(str(soup.encode("utf-8")))
 f.close()
 
 f= open('content.txt','r')
 lines = f.readlines()
 f.close()
 
-# for line in lines:
-#     if "Indian Rupee" in line:
-#         data = line.split()
-#         # print(data)
-#         value = ""
-#         for char in str(data[data.index("Indian")-1]):
-#             if char.isdigit() or char == ".":
-#                 value += char
-#         print(float(value))
-#         break
+for line in lines:
+    if "Indian Rupee" in line:
+        data = line.split()
+        print(f"{to_curr} in Indian Rupee = "+data[data.index("Indian")-1].split(">")[1])
+        break
